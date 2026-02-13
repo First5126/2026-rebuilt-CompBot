41c28e31d0675357bc9107fea759f1fa4644d151
SDPowerStrok3 <cole.howdeshell@gmail.com>
2026-02-13 15:03:47 -0600

    Set up some new methods

--- src/main/java/frc/robot/subsystems/FuelRollers.java
+++ src/main/java/frc/robot/subsystems/FuelRollers.java
@@ -1,8 +1,11 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class FuelRollers {
+public class FuelRollers extends SubsystemBase {
   private PWMSparkMax sparkMaxController;
 
   /** Creates the fuel rollers subsystem. */
@@ -10,13 +13,25 @@ public class FuelRollers {
     sparkMaxController = new PWMSparkMax(0);
   }
 
+  public Command startRollersCommand() {
+    return runOnce(() -> {
+      startRollers();
+    });
+  }
+
+  public Command stopRollersCommand() {
+    return runOnce(() -> {
+      stopRollers();
+    });
+  }
+
   /** Starts the rollers at intake speed. */
-  public void startRollers() {
+  private void startRollers() {
     sparkMaxController.set(-0.6);
   }
 
   /** Stops the rollers. */
-  public void stopRollers() {
+  private void stopRollers() {
     sparkMaxController.set(0);
   }
 }
