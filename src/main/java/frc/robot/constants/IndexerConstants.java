2f6b1b4a10b2629fab8164b1e52d43568a7e9300
Mark-Lua <s62711@parkhill.12.mo.us>
2026-02-26 20:02:09 -0600

    Spotless

--- src/main/java/frc/robot/constants/IndexerConstants.java
+++ src/main/java/frc/robot/constants/IndexerConstants.java
@@ -5,9 +5,9 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 import edu.wpi.first.units.measure.AngularVelocity;
 
 public class IndexerConstants {
-    public static double indexerKP = 5;
-    public static double spindexerKP = 5;
+  public static double indexerKP = 5;
+  public static double spindexerKP = 5;
 
-    public static AngularVelocity indexerSpeed = RotationsPerSecond.of(3);
-    public static AngularVelocity spindexerSpeed = RotationsPerSecond.of(3);
+  public static AngularVelocity indexerSpeed = RotationsPerSecond.of(3);
+  public static AngularVelocity spindexerSpeed = RotationsPerSecond.of(3);
 }
--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFXS;
 import com.ctre.phoenix6.signals.MotorArrangementValue;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.CANConstants;
@@ -14,51 +13,52 @@ import frc.robot.constants.IndexerConstants;
 
 public class Indexer extends SubsystemBase {
 
-    private TalonFXS m_indexerMotor = new TalonFXS(CANConstants.indexerMotor);
-    private TalonFXS m_spindexerMotor = new TalonFXS(CANConstants.spindexerMotor);
+  private TalonFXS m_indexerMotor = new TalonFXS(CANConstants.indexerMotor);
+  private TalonFXS m_spindexerMotor = new TalonFXS(CANConstants.spindexerMotor);
 
-    private VelocityVoltage m_indexerVelocityVoltage = new VelocityVoltage(0);
-    private VelocityVoltage m_spindexerVelocityVoltage = new VelocityVoltage(0);
-    private DutyCycleOut m_dutyCycleOut = new DutyCycleOut(0);
+  private VelocityVoltage m_indexerVelocityVoltage = new VelocityVoltage(0);
+  private VelocityVoltage m_spindexerVelocityVoltage = new VelocityVoltage(0);
+  private DutyCycleOut m_dutyCycleOut = new DutyCycleOut(0);
 
-    public Indexer() {
+  public Indexer() {
 
-        TalonFXSConfiguration m_indexerConfiguration = new TalonFXSConfiguration();
-        m_indexerConfiguration.Commutation.MotorArrangement = MotorArrangementValue.Disabled;
+    TalonFXSConfiguration m_indexerConfiguration = new TalonFXSConfiguration();
+    m_indexerConfiguration.Commutation.MotorArrangement = MotorArrangementValue.Disabled;
 
-        Slot0Configs m_indexerSlot0Configs = new Slot0Configs();
-        m_indexerSlot0Configs.kP = IndexerConstants.indexerKP;
+    Slot0Configs m_indexerSlot0Configs = new Slot0Configs();
+    m_indexerSlot0Configs.kP = IndexerConstants.indexerKP;
 
-        m_indexerConfiguration.Slot0 = m_indexerSlot0Configs;
+    m_indexerConfiguration.Slot0 = m_indexerSlot0Configs;
 
-        m_indexerMotor.getConfigurator().apply(m_indexerConfiguration);
+    m_indexerMotor.getConfigurator().apply(m_indexerConfiguration);
 
-        TalonFXSConfiguration m_spindexerConfiguration = new TalonFXSConfiguration();
-        m_spindexerConfiguration.Commutation.MotorArrangement = MotorArrangementValue.Disabled;
+    TalonFXSConfiguration m_spindexerConfiguration = new TalonFXSConfiguration();
+    m_spindexerConfiguration.Commutation.MotorArrangement = MotorArrangementValue.Disabled;
 
-        Slot0Configs m_spindexerSlot0Configs = new Slot0Configs();
-        m_spindexerSlot0Configs.kP = IndexerConstants.indexerKP;
+    Slot0Configs m_spindexerSlot0Configs = new Slot0Configs();
+    m_spindexerSlot0Configs.kP = IndexerConstants.indexerKP;
 
-        m_spindexerConfiguration.Slot0 = m_spindexerSlot0Configs;
+    m_spindexerConfiguration.Slot0 = m_spindexerSlot0Configs;
 
-        m_spindexerMotor.getConfigurator().apply(m_spindexerConfiguration);
-    }
+    m_spindexerMotor.getConfigurator().apply(m_spindexerConfiguration);
+  }
 
-    public Command startIndexing() {
-        return runOnce(() -> startMotors());
-    }
+  public Command startIndexing() {
+    return runOnce(() -> startMotors());
+  }
 
-    public Command stopIndexing() {
-        return runOnce(() -> stopMotors());
-    }
+  public Command stopIndexing() {
+    return runOnce(() -> stopMotors());
+  }
 
-    private void startMotors() {
-        m_indexerMotor.setControl(m_indexerVelocityVoltage.withVelocity(IndexerConstants.indexerSpeed));
-        m_spindexerMotor.setControl(m_spindexerVelocityVoltage.withVelocity(IndexerConstants.spindexerSpeed));
-    }
+  private void startMotors() {
+    m_indexerMotor.setControl(m_indexerVelocityVoltage.withVelocity(IndexerConstants.indexerSpeed));
+    m_spindexerMotor.setControl(
+        m_spindexerVelocityVoltage.withVelocity(IndexerConstants.spindexerSpeed));
+  }
 
-    private void stopMotors() {
-        m_indexerMotor.setControl(m_dutyCycleOut.withOutput(0));
-        m_spindexerMotor.setControl(m_dutyCycleOut.withOutput(0));
-    }
+  private void stopMotors() {
+    m_indexerMotor.setControl(m_dutyCycleOut.withOutput(0));
+    m_spindexerMotor.setControl(m_dutyCycleOut.withOutput(0));
+  }
 }
