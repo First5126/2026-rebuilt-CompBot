cf6ee2af484c53b2584d58397f1a7211e1adc2fc
Mark-Lua <s62711@parkhill.k12.mo.us>
2026-03-07 10:13:42 -0600

    Added Flywheel (#15)
    Co-authored-by: Mark-Lua <s62711@parkhill.12.mo.us>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,6 +19,7 @@ import frc.robot.controller.Driver;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandFactory;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.FlyWheel;
 import frc.robot.subsystems.ShootingMechanism;
 import frc.robot.subsystems.Turret;
 import frc.robot.vision.AprilTagLocalization;
@@ -48,6 +49,7 @@ public class RobotContainer {
 
   private Turret m_turret = new Turret();
   private Zones m_zones = new Zones(m_drivetrain::getPose2d);
+  private FlyWheel m_flyWheel = new FlyWheel();
 
   private ShootingMechanism m_shootingMechanism =
       new ShootingMechanism(m_turret, m_drivetrain, m_zones);
