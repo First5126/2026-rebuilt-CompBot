d20b9461007b0d9db73d7f80df88c41bb607862a
Mark-Lua <s62711@parkhill.12.mo.us>
2026-03-07 09:06:08 -0600

    Added Flywheel

---
+++ src/main/java/frc/robot/constants/FlyWheelConstants.java
@@ -0,0 +1,21 @@
+package frc.robot.constants;
+
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Distance;
+
+public class FlyWheelConstants {
+  public static double kP = 0.6;
+  public static double kI = 0;
+  public static double kD = 0.003;
+  public static double kS = 0.32;
+  public static double kV = 0.1168;
+  public static double kA = 0;
+
+  public static AngularVelocity flyWheelSpeed = RotationsPerSecond.of(-100);
+
+  public final static int gearRatio = 2;
+  public final static Distance radius = Meters.of(0.0508);
+}
