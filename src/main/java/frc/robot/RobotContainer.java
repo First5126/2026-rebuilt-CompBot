7e9afe8cb5b8468a2cdc5408669a2643ec4ec79e
RoWiz09 <139277027+RoWiz09@users.noreply.github.com>
2026-03-07 12:46:22 -0600

    Fix robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -49,7 +49,7 @@ public class RobotContainer {
   // Declare Subsystems Here
 
   private Turret m_turret = new Turret();
-  private Zones m_zones = new Zones(m_drivetrain::getPose2d);
+  // private Zones m_zones = new Zones(m_drivetrain::getPose2d);
   private FlyWheel m_flyWheel = new FlyWheel();
 
   private Hood m_hood = new Hood();
@@ -65,14 +65,14 @@ public class RobotContainer {
   public CommandFactory m_commandFactory =
       new CommandFactory(m_drivetrain, m_turret, m_zones, m_shootingMechanism);
 
-  private AprilTagLocalization m_aprilTagLocalization =
-      new AprilTagLocalization(
-          m_drivetrain::getPose2d,
-          m_drivetrain::resetPose,
-          m_drivetrain::addVisionMeasurement,
-          m_drivetrain,
-          photonDetails,
-          AprilTagLocalizationConstants.LIMELIGHT_DETAILS_RIGHT);
+  // private AprilTagLocalization m_aprilTagLocalization =
+  //     new AprilTagLocalization(
+  //         m_drivetrain::getPose2d,
+  //         m_drivetrain::resetPose,
+  //         m_drivetrain::addVisionMeasurement,
+  //         m_drivetrain,
+  //         photonDetails,
+  //         AprilTagLocalizationConstants.LIMELIGHT_DETAILS_RIGHT);
 
   private final SendableChooser<Command> autoChooser;
 
