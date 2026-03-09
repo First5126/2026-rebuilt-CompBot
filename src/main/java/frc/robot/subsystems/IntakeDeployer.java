392fa71a0705b95bf5b85960bbb35f3972c5f9ca
Brick219 <s86434@parkhill.k12.mo.us>
2026-03-09 16:46:36 -0500

    Fixed two errors with intakeDeployer.java

--- src/main/java/frc/robot/subsystems/IntakeDeployer.java
+++ src/main/java/frc/robot/subsystems/IntakeDeployer.java
@@ -18,9 +18,9 @@ import frc.robot.constants.IntakeDeployerConstants;
 public class IntakeDeployer extends SubsystemBase {
 
   private TalonFX m_intakeDeployerMotorRight =
-      new TalonFX(CANConstants.intakePiviotRight, CANConstants.Canivore2);
+      new TalonFX(CANConstants.intakeDeloyerMotor, CANConstants.Canivore2);
   private TalonFX m_intakeDeployerMotorLeft =
-      new TalonFX(CANConstants.intakePiviotLeft, CANConstants.Canivore2);
+      new TalonFX(CANConstants.intakeDeloyerMotor, CANConstants.Canivore2);
   private TalonFX m_intakeRollerMotor =
       new TalonFX(CANConstants.intakeWheelsMotor, CANConstants.Canivore2);
   final MotionMagicExpoVoltage m_request = new MotionMagicExpoVoltage(0);
