9c6e9153b91be26806e1e84a9026f588fef84c84
Mark-Lua <s62711@parkhill.12.mo.us>
2026-03-09 15:59:42 -0500

    added configure

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -82,13 +82,12 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+    Driver.init(m_drivetrain, m_aprilTagLocalization, m_commandFactory, m_turret, m_zones, m_indexer).configureBindings();
+
     // Turret Default Command
 
     // this.m_turret.setDefaultCommand(m_turret.rotateToPosition(() ->
     // m_shootingMechanism.getShootingSolution().predictedTurretAngle));
-
-    Driver.init(m_drivetrain, m_aprilTagLocalization, m_commandFactory, m_turret, m_zones, m_indexer)
-        .configureBindings();
 
     // Idle while the robot is disabled. This ensures the configured
     // neutral mode is applied to the drive motors while disabled.
