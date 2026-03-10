36fea3a3fd85a044a272f7c8189afc0c85cc48d1
RoWiz09 <139277027+RoWiz09@users.noreply.github.com>
2026-03-10 18:29:18 -0500

    Rezero on lower limit

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -18,12 +18,16 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.constants.CANConstants;
 import frc.robot.constants.HoodConstants;
 
 public class Hood extends SubsystemBase {
   private TalonFX m_hoodMotor;
   private CANdi m_CANdi;
+  private CANcoder m_hoodCANCoder;
+
+  private Trigger m_zeroTrigger;
 
   private Slot0Configs m_motorConfigs;
 
@@ -32,38 +36,47 @@ public class Hood extends SubsystemBase {
   public Hood() {
     m_hoodMotor = new TalonFX(CANConstants.hoodMotor, CANConstants.mechanismCanivore);
     m_CANdi = new CANdi(CANConstants.hoodCANdi, CANConstants.mechanismCanivore);
-
+    
+    m_hoodCANCoder = new CANcoder(CANConstants.hoodEncoder, CANConstants.mechanismCanivore);
+    
     TalonFXConfiguration talonConfiguration = new TalonFXConfiguration();
     talonConfiguration.Feedback.RotorToSensorRatio = 8.33;
     talonConfiguration.Feedback.SensorToMechanismRatio = 18;
     talonConfiguration.Feedback.withFusedCANcoder(
-        new CANcoder(CANConstants.hoodEncoder, CANConstants.mechanismCanivore));
-
+      m_hoodCANCoder);
+      
     m_hoodMotor.getConfigurator().apply(talonConfiguration);
-
+    
     // PID Configs
     m_motorConfigs = new Slot0Configs();
-
+    
     m_motorConfigs.kP = HoodConstants.kP;
     m_motorConfigs.kI = HoodConstants.kI;
     m_motorConfigs.kD = HoodConstants.kD;
     m_motorConfigs.kS = HoodConstants.kS;
     m_motorConfigs.kV = HoodConstants.kV;
-
+    
     m_hoodMotor.getConfigurator().apply(m_motorConfigs);
 
     // Hardware Limit Switches
     HardwareLimitSwitchConfigs limitSwitchConfigs = new HardwareLimitSwitchConfigs();
     limitSwitchConfigs.withReverseLimitRemoteCANdiS2(m_CANdi);
     limitSwitchConfigs.withForwardLimitRemoteCANdiS1(m_CANdi);
-
+    
     limitSwitchConfigs.withReverseLimitAutosetPositionEnable(true);
     limitSwitchConfigs.withReverseLimitAutosetPositionValue(Rotations.of(0));
-
+    
     m_hoodMotor.getConfigurator().apply(limitSwitchConfigs);
-
+    
     // Initalize the PositionVoltage request
     m_positionVoltageRequest = new PositionVoltage(0).withSlot(0);
+    
+    m_zeroTrigger = new Trigger(this::getLowerLimitValue);
+    m_zeroTrigger.onFalse(runOnce(() -> {m_hoodCANCoder.setPosition(Rotations.of(0.0));}));
+  }
+
+  private boolean getLowerLimitValue() {
+    return m_CANdi.getS1Closed().getValue();
   }
 
   @Override
