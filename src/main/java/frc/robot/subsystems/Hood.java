a1bd83a1c46981fe968ccb9ccb36de3279a5520d
RoWiz <robotwizard09@gmail.com>
2026-03-11 17:01:27 -0500

    Hard limits work now

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Rotations;
 
+import com.ctre.phoenix6.configs.DigitalInputsConfigs;
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -15,6 +17,8 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.CANdi;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.ReverseLimitValue;
+import com.ctre.phoenix6.signals.S1CloseStateValue;
+import com.ctre.phoenix6.signals.S2CloseStateValue;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -39,30 +43,37 @@ public class Hood extends SubsystemBase {
   public Hood() {
     m_hoodMotor = new TalonFX(CANConstants.hoodMotor, CANConstants.mechanismCanivore);
     m_CANdi = new CANdi(CANConstants.hoodCANdi, CANConstants.mechanismCanivore);
-    
+
     m_hoodCANCoder = new CANcoder(CANConstants.hoodEncoder, CANConstants.mechanismCanivore);
-    
+
+    // Mechanism Configuration
     TalonFXConfiguration talonConfiguration = new TalonFXConfiguration();
     talonConfiguration.Feedback.RotorToSensorRatio = 8.33;
     talonConfiguration.Feedback.SensorToMechanismRatio = 18;
-    talonConfiguration.Feedback.withFusedCANcoder(
-      m_hoodCANCoder);
-      
+    talonConfiguration.Feedback.withFusedCANcoder(m_hoodCANCoder);
+
     m_hoodMotor.getConfigurator().apply(talonConfiguration);
-    
+
     // PID Configs
     m_motorConfigs = new Slot0Configs();
-    
+
     m_motorConfigs.kP = HoodConstants.kP;
     m_motorConfigs.kI = HoodConstants.kI;
     m_motorConfigs.kD = HoodConstants.kD;
     m_motorConfigs.kS = HoodConstants.kS;
     m_motorConfigs.kV = HoodConstants.kV;
-    
+
     m_hoodMotor.getConfigurator().apply(m_motorConfigs);
 
     // Hardware Limit Switches
+    HardwareLimitSwitchConfigs hardConfigs = new HardwareLimitSwitchConfigs();
+    hardConfigs.withForwardLimitRemoteCANdiS2(m_CANdi);
+    hardConfigs.withForwardLimitEnable(true);
 
+    hardConfigs.withReverseLimitRemoteCANdiS1(m_CANdi);
+    hardConfigs.withReverseLimitEnable(true);
+
+    m_hoodMotor.getConfigurator().apply(hardConfigs);
 
     // Software Limit Switches
     SoftwareLimitSwitchConfigs softConfigs = new SoftwareLimitSwitchConfigs();
@@ -74,15 +85,25 @@ public class Hood extends SubsystemBase {
 
     m_hoodMotor.getConfigurator().apply(softConfigs);
 
+    // CANdi Configs
+    DigitalInputsConfigs inputConfigs = new DigitalInputsConfigs();
+    inputConfigs.withS1CloseState(S1CloseStateValue.CloseWhenLow);
+    inputConfigs.withS2CloseState(S2CloseStateValue.CloseWhenLow);
+
+    m_CANdi.getConfigurator().apply(inputConfigs);
+
     // Initalize the PositionVoltage request
     m_positionVoltageRequest = new PositionVoltage(0).withSlot(0);
 
     SmartDashboard.putNumber("Hood Angle (Deg)", 0);
-    
+
     m_zeroTrigger = new Trigger(this::getLowerLimitValue);
-    m_zeroTrigger.onTrue(runOnce(
-      () -> {m_hoodCANCoder.setPosition(Rotations.of(0.0));})
-      .ignoringDisable(true));
+    m_zeroTrigger.onTrue(
+        runOnce(
+                () -> {
+                  m_hoodCANCoder.setPosition(Rotations.of(0.0));
+                })
+            .ignoringDisable(true));
   }
 
   private boolean getLowerLimitValue() {
