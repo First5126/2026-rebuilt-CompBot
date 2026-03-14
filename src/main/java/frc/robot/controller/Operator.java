812bc7d4d8849daa273c08515f9f28d76d88c324
SDPowerStrok3 <cole.howdeshell@gmail.com>
2026-03-14 09:32:34 -0500

    Debug button for testing

--- src/main/java/frc/robot/controller/Operator.java
+++ src/main/java/frc/robot/controller/Operator.java
@@ -1,8 +1,11 @@
 package frc.robot.controller;
 
+import static edu.wpi.first.units.Units.Degrees;
+
 import frc.robot.FMS.Zones;
 import frc.robot.constants.ControllerConstants;
 import frc.robot.subsystems.CommandFactory;
+import frc.robot.subsystems.Hood;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Turret;
 import frc.robot.vision.AprilTagLocalization;
@@ -18,6 +21,8 @@ public class Operator extends CustomXboxController implements Controller {
   @Getter @Setter private Intake intake;
   @Getter @Setter private Turret turret;
   @Getter @Setter private Zones zone;
+  @Getter @Setter private Hood hood;
+
 
   // Private constructor to prevent instantiation from outside
   private Operator() {
@@ -51,6 +56,8 @@ public class Operator extends CustomXboxController implements Controller {
         .onFalse(zone.setShootingOverideCommand(false));
 
     this.b().onTrue(commandFactory.goUnderTrenchCommand());
+
+    this.x().onTrue(hood.setPosition(Degrees.of(20)));
 
     return this;
   }
