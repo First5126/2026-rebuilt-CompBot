8d09b8d11e2e448d1f4d35381be4cf093fe0397d
Mark-Lua <s62711@parkhill.12.mo.us>
2026-03-25 19:29:11 -0500

    Added calculation delay

--- src/main/java/frc/robot/constants/ShootingMechanismConstants.java
+++ src/main/java/frc/robot/constants/ShootingMechanismConstants.java
@@ -32,6 +32,7 @@ public class ShootingMechanismConstants {
   public static final Angle hoodMaximumError = Degree.of(2);
   public static final AngularVelocity flyWheelMaximumError = RotationsPerSecond.of(2);
   public static final Time mechanismDelay = Milliseconds.of(52.647);
+  public static final Time computationDelay = Milliseconds.of(20);
   public static final int updateCounter = 2;
 
   // Distance first then Time
--- src/main/java/frc/robot/subsystems/CommandFactory.java
+++ src/main/java/frc/robot/subsystems/CommandFactory.java
@@ -223,8 +223,7 @@ public class CommandFactory {
 
   public Command startFlywheelWithSolution() {
     return m_flyWheel
-        .setSpeedWithSolution(m_shootingMechanism::getShootingSolution)
-        .alongWith(agitateIntake());
+        .setSpeedWithSolution(m_shootingMechanism::getShootingSolution);
   }
 
   public Command setTurretToZero() {
